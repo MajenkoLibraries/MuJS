824a8674e3c5acedaae513f53826ffa73bfb7f07
Matt Jenkins <matt@majenko.co.uk>
2016-05-11 12:21:55 +0100

    Added some common global symbols

--- js_core_io.cpp
+++ js_core_io.cpp
@@ -28,4 +28,55 @@ void js_core_io_init(js_State *J) {
 
     js_newcfunction(J, js_core_io_digitalRead, "digitalRead", 1);
     js_setglobal(J, "digitalRead");
+
+    js_pushnumber(J, 0);
+    js_setglobal(J, "INPUT");
+
+    js_pushnumber(J, 1);
+    js_setglobal(J, "OUTPUT");
+
+    js_pushnumber(J, 2);
+    js_setglobal(J, "OPEN");
+
+    js_pushnumber(J, 3);
+    js_setglobal(J, "INPUT_PULLUP");
+
+    js_pushnumber(J, 4);
+    js_setglobal(J, "INPUT_PULLDOWN");
+
+    js_pushnumber(J, 5);
+    js_setglobal(J, "INPUT_PULLUPDOWN");
+
+    js_pushnumber(J, 0);
+    js_setglobal(J, "LOW");
+
+    js_pushnumber(J, 1);
+    js_setglobal(J, "HIGH");
+
+    js_pushnumber(J, 1);
+    js_setglobal(J, "CHANGE");
+
+    js_pushnumber(J, 2);
+    js_setglobal(J, "FALLING");
+
+    js_pushnumber(J, 3);
+    js_setglobal(J, "RISING");
+
+#ifdef PIN_LED1
+    js_pushnumber(J, PIN_LED1);
+    js_setglobal(J, "PIN_LED1");
+#endif
+#ifdef PIN_LED2
+    js_pushnumber(J, PIN_LED2);
+    js_setglobal(J, "PIN_LED2");
+#endif
+#ifdef PIN_LED3
+    js_pushnumber(J, PIN_LED3);
+    js_setglobal(J, "PIN_LED3");
+#endif
+#ifdef PIN_LED4
+    js_pushnumber(J, PIN_LED4);
+    js_setglobal(J, "PIN_LED4");
+#endif
+
 }
